7c98b8d1acc647b286d124d35d7f50db57a47847
Viateur <akimanaviateur94@gmail.com>
2024-01-26 20:52:41 +0200

    spotify integration

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import spotify from 'spotify-web-api-js';
 
 function App() {
   const [token,setToken] = useState(null);
@@ -11,6 +12,9 @@ function App() {
   const token = hash.access_token;
   if(token){
     setToken(token);
+    spotify.getMe().then((user)=>{
+      console.log("viateur:",user)
+    })
   }
   },[])
   return (
