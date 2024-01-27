3aef2d175f6d252f668434efe0d2a50428598656
Viateur <akimanaviateur94@gmail.com>
2024-01-27 16:11:29 +0200

    working on homepage

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
 import "./footer.css";
 
 const Footer = () => {
